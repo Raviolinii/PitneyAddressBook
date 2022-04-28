3c658d29c1bb592a23f1e0d5ce78d60b6e382a29
Krzysztof Garlacz <54943478+Raviolinii@users.noreply.github.com>
2022-04-28 14:35:48 +0200

    AddressBook
    -Address book
    -Address
    -Address book repository

---
+++ PitneyAddressBook/PitneyAddressBook/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace PitneyAddressBook.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string AddressName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public char[] PostalCode { get; set; }
+    }
+}
---
+++ PitneyAddressBook/PitneyAddressBook/Models/AddressBook.cs
@@ -0,0 +1,14 @@
+﻿namespace PitneyAddressBook.Models
+{
+    public class AddressBook
+    {
+        public List<Address> addresses { get; set; }
+        public Address? LastAddress { get; set; }
+
+        public AddressBook()
+        {
+            addresses = new List<Address>();
+            LastAddress = null;
+        }
+    }
+}
